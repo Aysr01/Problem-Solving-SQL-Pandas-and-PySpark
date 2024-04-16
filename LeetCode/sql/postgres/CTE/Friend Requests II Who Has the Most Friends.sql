68f62418b8f6d1405f3db2593249ba0f808f9671
Aysr01 <sarab.ayoub@inemail.ine.inpt.ma>
2024-04-16 18:41:37 +0100

    Solve <<Friend Requests II Who Has the Most Friends>> on LeetCode

---
+++ LeetCode/sql/postgres/CTE/Friend Requests II Who Has the Most Friends.sql
@@ -0,0 +1,13 @@
+WITH user_friends_count AS (
+    SELECT requester_id AS user_id
+    FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS user_id
+    FROM RequestAccepted
+)
+
+SELECT user_id AS id, COUNT(user_id) AS num
+FROM user_friends_count
+GROUP BY user_id
+ORDER BY num DESC
+LIMIT 1
