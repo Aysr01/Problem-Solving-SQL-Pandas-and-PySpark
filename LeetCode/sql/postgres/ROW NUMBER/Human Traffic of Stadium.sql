a0887695a2adf927d498cae868e0a19b1e49de9e
Aysr01 <sarab.ayoub@inemail.ine.inpt.ma>
2024-04-17 12:08:50 +0100

    Solve <<Human Traffic of Stadium>> problem on leetcode

---
+++ LeetCode/sql/postgres/ROW NUMBER/Human Traffic of Stadium.sql
@@ -0,0 +1,23 @@
+WITH cte AS(
+    SELECT 
+        id,
+        visit_date,
+        people,
+        id - ROW_NUMBER() OVER() as lt_100_count
+        -- The field above counts the number of rows where the number of visitors
+        -- is less than 100
+    FROM Stadium
+    WHERE people >= 100
+)
+
+SELECT
+    id,
+    visit_date,
+    people
+FROM cte
+WHERE lt_100_count IN (
+    SELECT lt_100_count
+    FROM cte
+    GROUP BY lt_100_count
+    HAVING COUNT(lt_100_count) >= 3
+)
