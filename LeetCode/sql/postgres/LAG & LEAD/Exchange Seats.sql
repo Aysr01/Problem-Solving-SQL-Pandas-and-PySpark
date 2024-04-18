abd3db36c066a8218d9e2b045eed6acbc00fb58e
Aysr01 <sarab.ayoub@inemail.ine.inpt.ma>
2024-04-18 02:44:09 +0100

    LAG and LEAD problems

---
+++ LeetCode/sql/postgres/LAG & LEAD/Exchange Seats.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+SELECT
+    id,
+    CASE
+        WHEN id % 2 = 0 THEN LAG(student) OVER()
+        WHEN LEAD(student) OVER() IS NOT NULL THEN LEAD(student) OVER()
+        ELSE student
+    END AS student
+FROM
+    Seat
