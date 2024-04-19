7371d5102bb885808ddb0ea015cffdda0625ccff
Aysr01 <sarab.ayoub@inemail.ine.inpt.ma>
2024-04-20 00:49:52 +0100

    Solved <<Customers Who Bought All Products.sql>>

---
+++ LeetCode/sql/postgres/COUNT/Customers Who Bought All Products.sql
@@ -0,0 +1,9 @@
+-- GROUP BY, HAVING, COUNT, DISTINCT
+SELECT 
+    customer_id
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (
+    SELECT COUNT(product_key)
+    FROM Product
+)
