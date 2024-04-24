b5ed144a8e386afb709b3d9c754f1a7fa50708d3
Aysr01 <sarab.ayoub@inemail.ine.inpt.ma>
2024-04-24 14:51:49 +0100

    Solve Workers With The Highest Salaries Problem

---
+++ StrataScratch/pyspark/RANK/Workers With The Highest Salaries.py
@@ -0,0 +1,17 @@
+# Import your libraries
+import pyspark
+from pyspark.sql import Window
+from pyspark.sql.functions import avg, col, rank
+
+# Start writing code
+window = Window.orderBy(col("salary").desc())
+               
+highest_paid_titles = worker.withColumn("rk", rank().over(window)) \
+    .where(col("rk") == 1) \
+    .join(title, worker.worker_id == title.worker_ref_id, "inner") \
+    .select(
+        col("worker_title").alias("best_paid_title")
+        ) \
+    .distinct()
+# To validate your solution, convert your final pySpark df to a pandas df
+highest_paid_titles.toPandas()
